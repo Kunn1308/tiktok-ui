7ed7577b07828b7f825c1217ec97a7f4ceec5516
Minh Ngọc <ngockunn13801@gmail.com>
2024-04-09 15:01:37 +0700

    Cài đặt và cấu hình Prettier

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import Button from '~/component/Button';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <Button />
-    </div>
-  );
+    return (
+        <div className="App">
+            <Button />
+        </div>
+    );
 }
 
 export default App;
--- src/component/Button/index.js
+++ src/component/Button/index.js
@@ -1,8 +1,6 @@
-import myAlert from "~/MyAlert";
-function Button(){
-    return (
-        <button onClick={myAlert}>Click me</button>
-    )
+import myAlert from '~/MyAlert';
+function Button() {
+    return <button onClick={myAlert}>Click me</button>;
 }
 
-export default Button
+export default Button;
